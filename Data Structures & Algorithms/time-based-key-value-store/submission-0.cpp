3ffb04844f75df6470e9b297d2001538c8dd653e
r14922134 <134042948+justin3728@users.noreply.github.com>
2026-09-02 11:44:53 +0800

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,34 @@
+class TimeMap {
+public:
+
+    map<string, vector<pair<int, string>>> mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        int l = 0;
+        int r = mp[key].size() - 1;
+
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+
+            if(mp[key][m].first == timestamp) {
+                return mp[key][m].second;
+            }
+            else if(mp[key][m].first < timestamp) {
+                l = m + 1;
+            }
+            else {
+                r = m - 1;
+            }
+        }
+        
+        //不一定有等於 所以是找最大的timestamp <= target
+        return r < 0 ? "" : mp[key][r].second;
+    }
+};
